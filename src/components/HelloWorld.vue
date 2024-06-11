6922054cb725937af56b9962139b80fd22a79170
Geister <walkerolivier@gmail.com>
2024-06-11 22:09:04 +0200

    modif affichage + affichage stats de base

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,6 +41,14 @@
 					</div>
 				</div>
 			</div>
+			<div class="base_stats">
+				Base Stats
+				<div>HP : {{ api_result.stats.hp }}</div>
+				<div>ATK : {{ api_result.stats.atk }}</div>
+				<div>DEF : {{ api_result.stats.def }}</div>
+				<div>ATK Spe : {{ api_result.stats.spe_atk }}</div>
+				<div>DEF Spe : {{ api_result.stats.spe_def }}</div>
+			</div>
 		</div>
 		<button @click="addPokemonToTeam()">Ajouter à l'équipe</button>
 		<button @click="addPokemonAsTarget()">Ajouter en tant que cible</button>
@@ -49,77 +57,97 @@
 	<hr />
 
 	<h2>Personnal team</h2>
-	<div class="d-flex align-items-center text-center" style="gap: 20px; border: 1px solid #000; margin-bottom: 10px;" v-for="pokemon in personnal_team" :key="pokemon.pokedex_id">
-		<div>{{ pokemon.name.fr }}<br /><img class="sprite" :src="pokemon.sprites.regular" alt=""></div>
-		<div>
-			Types :
-			<div class="d-flex align-items-center justify-content-center" style="gap: 5px" v-for="apiType in pokemon.types" :key="apiType.name">
-				{{ apiType.name }}
-				<img class="img-type" :src="apiType.image">
+	<div v-for="pokemon in personnal_team" :key="pokemon.pokedex_id" style="border: 1px solid #000; margin-bottom: 10px;">
+		<div class="d-flex align-items-center text-center" style="gap: 20px;">
+			<div>
+				{{ pokemon.name.fr }}<br />
+				<img class="sprite" :src="pokemon.sprites.regular" alt="">
 			</div>
+			<div>
+				Types :
+				<div class="d-flex align-items-center justify-content-center" style="gap: 5px" v-for="apiType in pokemon.types" :key="apiType.name">
+					{{ apiType.name }}
+					<img class="img-type" :src="apiType.image">
+				</div>
+			</div>
+			<div>
+				IV (BASE STAT)
+				<div class="ivs">
+					<div>PV : <input type="text" v-model="pokemon.ivs.hp"> ({{ pokemon.stats.hp }})</div>
+					<div>ATK : <input type="text" v-model="pokemon.ivs.atk"> ({{ pokemon.stats.atk }})</div>
+					<div>DEF : <input type="text" v-model="pokemon.ivs.def"> ({{ pokemon.stats.def }})</div>
+					<div>ATKSPE : <input type="text" v-model="pokemon.ivs.atk_spe"> ({{ pokemon.stats.spe_atk }})</div>
+					<div>DEFSPE : <input type="text" v-model="pokemon.ivs.def_spe"> ({{ pokemon.stats.spe_def }})</div>
+					<div>SPEED : <input type="text" v-model="pokemon.ivs.speed"> ({{ pokemon.stats.vit }})</div>
+				</div>
+			</div>
+			<button @click="removePokemonFromTeam(pokemon.pokedex_id)">X</button>
 		</div>
 		<div class="resistance">
-			Résistances
-			<div v-for="resistance in pokemon.resistances" :key="resistance.name">
-				<div v-if="resistance.multiplier < 1">
-					{{ resistance.name }} : {{ resistance.multiplier }}
-				</div>
-			</div>
+			Résistances :
+			<span v-for="resistance in pokemon.resistances" :key="resistance.name">
+				<span v-if="resistance.multiplier < 1">
+					| {{ resistance.name }} : {{ resistance.multiplier }}
+				</span>
+			</span>
 		</div>
-
 		<div class="faiblesse">
-			Faiblesses
-			<div v-for="resistance in pokemon.resistances" :key="resistance.name">
-				<div v-if="resistance.multiplier > 1">
-					{{ resistance.name }} : {{ resistance.multiplier }}
-				</div>
-			</div>
+			Faiblesses :
+			<span v-for="resistance in pokemon.resistances" :key="resistance.name">
+				<span v-if="resistance.multiplier > 1">
+					| {{ resistance.name }} : {{ resistance.multiplier }}
+				</span>
+			</span>
 		</div>
 
-		<div>
-			IV
-			<div class="ivs">
-				<div>PV : <input type="text" v-model="pokemon.ivs.hp"></div>
-				<div>ATK : <input type="text" v-model="pokemon.ivs.atk"></div>
-				<div>DEF : <input type="text" v-model="pokemon.ivs.def"></div>
-				<div>ATKSPE : <input type="text" v-model="pokemon.ivs.atk_spe"></div>
-				<div>DEFSPE : <input type="text" v-model="pokemon.ivs.def_spe"></div>
-				<div>SPEED : <input type="text" v-model="pokemon.ivs.speed"></div>
-			</div>
-		</div>
-		<button @click="removePokemonFromTeam(pokemon.pokedex_id)">X</button>
 	</div>
 
 	<hr />
 
 	<h2>Targets</h2>
-	<div class="d-flex align-items-center text-center" style="gap: 20px; border: 1px solid #000; margin-bottom: 10px;" v-for="pokemon in targets" :key="pokemon.pokedex_id">
-		<div>{{ pokemon.name.fr }}<br /><img class="sprite" :src="pokemon.sprites.regular" alt=""></div>
-		<div>
-			Types :
-			<div class="d-flex align-items-center justify-content-center" style="gap: 5px" v-for="apiType in pokemon.types" :key="apiType.name">
-				{{ apiType.name }}
-				<img class="img-type" :src="apiType.image">
+	<div v-for="pokemon in targets" :key="pokemon.pokedex_id" style="border: 1px solid #000; margin-bottom: 10px;">
+		<div class="d-flex align-items-center text-center" style="gap: 20px;">
+			<div>
+				{{ pokemon.name.fr }}<br />
+				<img class="sprite" :src="pokemon.sprites.regular" alt="">
 			</div>
+			<div>
+				Types :
+				<div class="d-flex align-items-center justify-content-center" style="gap: 5px" v-for="apiType in pokemon.types" :key="apiType.name">
+					{{ apiType.name }}
+					<img class="img-type" :src="apiType.image">
+				</div>
+			</div>
+			<div>
+				IV (BASE STAT)
+				<div class="ivs">
+					<div>PV : <input type="text" v-model="pokemon.ivs.hp"> ({{ pokemon.stats.hp }})</div>
+					<div>ATK : <input type="text" v-model="pokemon.ivs.atk"> ({{ pokemon.stats.atk }})</div>
+					<div>DEF : <input type="text" v-model="pokemon.ivs.def"> ({{ pokemon.stats.def }})</div>
+					<div>ATKSPE : <input type="text" v-model="pokemon.ivs.atk_spe"> ({{ pokemon.stats.spe_atk }})</div>
+					<div>DEFSPE : <input type="text" v-model="pokemon.ivs.def_spe"> ({{ pokemon.stats.spe_def }})</div>
+					<div>SPEED : <input type="text" v-model="pokemon.ivs.speed"> ({{ pokemon.stats.vit }})</div>
+				</div>
+			</div>
+			<button @click="removePokemonFromTarget(pokemon.pokedex_id)">X</button>
 		</div>
 		<div class="resistance">
 			Résistances
-			<div v-for="resistance in pokemon.resistances" :key="resistance.name">
-				<div v-if="resistance.multiplier < 1">
-					{{ resistance.name }} : {{ resistance.multiplier }}
-				</div>
-			</div>
+			<span v-for="resistance in pokemon.resistances" :key="resistance.name">
+				<span v-if="resistance.multiplier < 1">
+					| {{ resistance.name }} : {{ resistance.multiplier }}
+				</span>
+			</span>
 		</div>
 
 		<div class="faiblesse">
 			Faiblesses
-			<div v-for="resistance in pokemon.resistances" :key="resistance.name">
-				<div v-if="resistance.multiplier > 1">
-					{{ resistance.name }} : {{ resistance.multiplier }}
-				</div>
-			</div>
+			<span v-for="resistance in pokemon.resistances" :key="resistance.name">
+				<span v-if="resistance.multiplier > 1">
+					| {{ resistance.name }} : {{ resistance.multiplier }}
+				</span>
+			</span>
 		</div>
-		<button @click="removePokemonFromTarget(pokemon.pokedex_id)">X</button>
 	</div>
 </template>
 
